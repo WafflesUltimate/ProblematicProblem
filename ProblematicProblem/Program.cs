73ab5cdbad421cbe2fc70ef18726ebfed164ca3f
David Armstrong <wafflesultimate1@gmail.com>
2023-10-20 03:47:41 -0700

    completed Exercise

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,84 +1,165 @@
-System;
-using System.Collections.Generic
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
-ProblematicProblem
+namespace ProblematicProblem
 {
-    Program class
+    public class Program
     {
-        Random rng;
-static bool cont = true;
-static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+        //Universal method Declarations
+        public static bool StringToBoolValidater(string input) 
+        {
+            input = input.ToLower();
+
+            if (input.Contains("yes") || input.Contains("sure") || input.Contains("keep") || input.Contains("yep") || input.Contains("yeah")) { return true; }
+            else { return false; } 
+        }
+
+        public static string StringCleaner(string input)
+        {
+           input = input.ToLower();
+           input = input.Trim();
+   
+           return input;
+        }
+
+        //Universal variable delcarations.
+        static bool cont = true;
+        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
+        static string userName;
+        static int userAge;
+        static string stringValidater;
+        static int intValidater = 0;
+        static bool boolValidater;
+
         static void Main(string[] args)
-{
-    Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.Write("We are going to need your information first! What is your name? ");
-    string userName = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("What is your age? ");
-    int userAge = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-    bool seeList = bool.Parse(Console.ReadLine());
-    if (seeList)
-    {
-        foreach (string activity in activities)
-        {
-            Console.Write($"{activity} ");
-            Thread.Sleep(250);
-        }
-        Console.WriteLine();
-        Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-        bool addToList = bool.Parse(Console.ReadLine());
-        Console.WriteLine();
-        while (addToList)
-        {
-            Console.Write("What would you like to add? ");
-            string userAddition = Console.ReadLine();
-            activities.Add(userAddition);
-            foreach (string activity activities)
-            {
-                Console.Write($"{activity} ");
-                Thread.Sleep(250);
-            }
+         {
+            //Greeter
+            Console.WriteLine("Welcome to the activities database!");
+
+            //name getter
+            Console.WriteLine("Before we continue, we are going to need some information first! What is your first name?");
+            userName = StringCleaner(Console.ReadLine());
+            Console.WriteLine($"Hello, {userName}!");
             Console.WriteLine();
-            Console.WriteLine("Would you like to add more? yes/no: ");
-            string addToList = bool.Parse(Console.ReadLine());
-        }
+
+            //age getter
+            Console.WriteLine($"How old are you?");
+            do{
+                stringValidater = Console.ReadLine();
+                boolValidater = int.TryParse(stringValidater, out intValidater);
+
+                if (boolValidater) { userAge = int.Parse(stringValidater); }
+                else { Console.WriteLine($"I'm sorry but I cant work with {stringValidater}. Please input a valid age."); }
+
+                if(userAge > 100 || userAge < 10) 
+                { Console.WriteLine($"I'm sorry but I can't work with {userAge}. Please input a valid age for this database."); boolValidater = false; }
+
+                Console.WriteLine();
+
+            }while (boolValidater == false);
+
+            //see list of activities prompt
+            Console.Write($"{userName} would you like to see the current list of activities? Sure/No thanks: ");
+            boolValidater = StringToBoolValidater(Console.ReadLine());
+
+            //activites loops
+            if (boolValidater)
+            {
+                //activity printer
+                Console.WriteLine($"Okay! Here is the list of activities:");
+                foreach (string activity in activities)
+                {
+                    Console.WriteLine($" - {activity}");
+                    Thread.Sleep(250);
+                }
+                Console.WriteLine();
+
+                //activity adder prompt
+                Console.Write("Would you like to add any activities? yes/no: ");
+                boolValidater = StringToBoolValidater(Console.ReadLine());
+                Console.WriteLine();
+
+                //activity adder
+                while (boolValidater)
+                {
+                    Console.WriteLine("What would you like to add?");
+                    string userAddition = Console.ReadLine();
+                    userAddition = StringCleaner(userAddition);
+
+                    activities.Add(userAddition);
+                    Console.WriteLine($"Okay! Here is the new list of activities:");
+                    foreach (string activity in activities)
+                    {
+                        Console.WriteLine($" - {activity}");
+                        Thread.Sleep(250);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Would you like to add more? yes/no: ");
+                    boolValidater = StringToBoolValidater(Console.ReadLine());
+                    Console.WriteLine();
+                }
+            }
+            //random activity generator prompt
+            Console.WriteLine($"{userName} would you like to access the random activity generater? Sure/No thanks: ");
+            Console.WriteLine("(Warning saying no exits the application.)");
+            boolValidater = StringToBoolValidater(Console.ReadLine());
+
+            if (boolValidater)
+            {
+                //fluff
+                Console.Write("Connecting to the database.");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write(". ");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
+                Console.Write("Choosing your random activity.");
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.Write(". ");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
+
+                //random variable creation 
+                Random rng = new Random();
+                int randomNumber;
+                string randomActivity;
+
+                ////random variable assignment
+                randomNumber = rng.Next(activities.Count);
+                randomActivity = activities[randomNumber];
+
+                while (userAge > 21 && randomActivity == "Wine Tasting")
+                {
+                     randomNumber = rng.Next(activities.Count);
+                     randomActivity = activities[randomNumber];
+                }
+
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                boolValidater = StringToBoolValidater(Console.ReadLine());
+
+                while (boolValidater == false)
+                {
+                    randomNumber = rng.Next(activities.Count);
+                    randomActivity = activities[randomNumber];
+
+                    Console.Write($"Okay {userName}, your new random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                    Console.WriteLine();
+                    boolValidater = StringToBoolValidater(Console.ReadLine());
+                }
+                Console.WriteLine($"Have a nice {randomActivity} day.");
+            }
+         }
+
     }
 
-    while (cont)
-    {
-        Console.Write("Connecting to the database");
-        for (int i = 0; i < 10; i++)
-        {
-            Console.Write(". ");
-            Thread.Sleep(500);
-        }
-        Console.WriteLine()
-                Console.Write("Choosing your random activity");
-        for (int i = 0; i < 9; i++)
-        {
-            Console.Write(". ");
-            Thread.Sleep(500);
-        }
-        Console.WriteLine()
-                int randomNumber = rng.Next(activities.Count);
-        string randomActivity = activities[randomNumber]
-                if (userAge > 21 && randomActivity == "Wine Tasting")
-        {
-            Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-            Console.WriteLine("Pick something else!");
-            activities.Remove(randomActivity);
-            string randomNumber = rng.Next(activities.Count);
-            string randomActivity = activities[randomNumber];
-        }
-        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
-        bool cont = bool.Parse(Console.ReadLine());
-    }
 }
-    }
-}}
+    
